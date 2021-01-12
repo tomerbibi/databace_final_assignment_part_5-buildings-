6ba19254b8dcb29d349a14ab4a8de1d868fc31c5
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2021-01-12 23:46:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_part_5__buildings_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = File.OpenText("Test_part_5_(buildings).txt");
+            string connection_string = reader.ReadToEnd();
+            log4net.ILog my_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            using (var con = new NpgsqlConnection(connection_string))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "call add_500_to_salary_in_loop()";
+                    NpgsqlCommand command = new NpgsqlCommand(query, con);
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+                catch(Exception)
+                {
+                    my_logger.Error("the stored procedure wa call add_500_to_salary_in_loop() was not exacuted");
+                }
+            }
+        }
+    }
+}
